d1d4a1436c18c7355034fe91d141358f8b958a5e
Max <Max@ASUS-X455LA>
2015-08-05 20:07:52 -0600

    Se comienza a trabajar en la Implementacion

---
+++ CONTA/src/Implementacion/Movimiento.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Implementacion;
+
+/**
+ *
+ * @author Max
+ */
+public class Movimiento {
+    
+    public String Codigo;
+    public Integer Monto;
+    
+    public Movimiento(String Codigo, Integer Monto){
+        this.Codigo = Codigo;
+        this.Monto = Monto;
+    }
+}
