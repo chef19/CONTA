da649f8c754c53bdb7e1583b5b96b8f9677631df
Max <Max@ASUS-X455LA>
2015-08-05 19:52:48 -0600

    Se crean todos los JFrame necesarios

--- CONTA/src/interfaz/AcreditariosBanco.java
+++ CONTA/src/interfaz/AcreditariosBanco.java
@@ -9,12 +9,12 @@ package interfaz;
  *
  * @author ANDRES MS
  */
-public class frame1 extends javax.swing.JFrame {
+public class AcreditariosBanco extends javax.swing.JFrame {
 
     /**
-     * Creates new form frame1
+     * Creates new form frame3
      */
-    public frame1() {
+    public AcreditariosBanco() {
         initComponents();
     }
 
@@ -60,20 +60,21 @@ public class frame1 extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(frame1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AcreditariosBanco.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(frame1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AcreditariosBanco.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(frame1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AcreditariosBanco.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(frame1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AcreditariosBanco.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new frame1().setVisible(true);
+                new AcreditariosBanco().setVisible(true);
             }
         });
     }
--- CONTA/src/interfaz/DebitariosCompañia.java
+++ CONTA/src/interfaz/DebitariosCompañia.java
@@ -9,12 +9,12 @@ package interfaz;
  *
  * @author ANDRES MS
  */
-public class frame4 extends javax.swing.JFrame {
+public class DebitariosCompañia extends javax.swing.JFrame {
 
     /**
-     * Creates new form frame4
+     * Creates new form frame2
      */
-    public frame4() {
+    public DebitariosCompañia() {
         initComponents();
     }
 
@@ -60,20 +60,21 @@ public class frame4 extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(frame4.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DebitariosCompañia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(frame4.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DebitariosCompañia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(frame4.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DebitariosCompañia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(frame4.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DebitariosCompañia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new frame4().setVisible(true);
+                new DebitariosCompañia().setVisible(true);
             }
         });
     }
